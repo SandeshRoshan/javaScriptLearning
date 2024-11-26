5d27a53c4672e45e76a3ce6fca981d39abe31df9
SandeshRoshan <srmilaptop@outlook.com>
2024-11-26 20:36:01 +0530

    object manupulation

--- first.js
+++ first.js
@@ -1,2 +1,22 @@
-let a = 10;
-console.log("in bracket", a);
+
+const student = {
+    fullName : "Sandesh Roshan",
+    age : 23,
+    isPass : true
+}
+
+// let student;
+
+console.log(student?.age === null ? "true":"false");
+console.log(student.age);
+student.age =24;
+
+console.log(student.age);
+student.age = student.age +1;
+console.log(student.age);
+
+student.fullName = student.fullName +1;
+console.log(student.fullName);
+
+student.fullName = student.fullName -1;
+console.log(student.fullName);
